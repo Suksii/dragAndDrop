01f4304b1fc4382dfa4ce474cfa72e557d91a5a4
unknown <ramovic225@gmail.com>
2024-12-07 10:57:36 +0100

    cursors for onMouseUp and onMouseDown added

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { TaskProps } from "./types/types";
 
 const Card = ({ title, description }: TaskProps) => {
+  const [isGrabbing, setIsGrabbing] = useState(false);
+
   return (
-    <div className="min-w-44 w-full h-fit shadow-lg bg-gray-700 rounded-xl p-6">
+    <div
+      className={`min-w-44 w-full h-fit shadow-lg bg-gray-700 rounded-xl p-6 ${
+        isGrabbing ? "cursor-grab" : "cursor-grabbing"
+      } `}
+      onMouseUp={() => setIsGrabbing(true)}
+      onMouseDown={() => setIsGrabbing(false)}
+    >
       <h1 className="text-gray-50 text-2xl pb-4">{title}</h1>
       <p className="text-gray-50 text-lg">{description}</p>
     </div>
